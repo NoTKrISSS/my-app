a8043490900ac4bfa088785f47dc2a7b45e8e50e
NoTKrISSS <46658821+NoTKrISSS@users.noreply.github.com>
2021-10-16 17:51:30 +0300

    bubble fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,62 @@
+import { Component } from "react";
 import Baloons from "./Components/Baloons";
-
-function App() {
-  return (
-    <>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  handlescore = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
       <div className="container">
-        <div>x</div>
-        <Baloons />
-        <Baloons />
-        <Baloons />
-        <Baloons />
-        <Baloons />
-        <Baloons />
-        <Baloons />
-        <Baloons />
-        <Baloons />
-        <Baloons />
+        <div>{this.state.count}</div>
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
+        <Baloons handlescore={this.handlescore} />
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Components/Baloons.jsx
+++ src/Components/Baloons.jsx
@@ -2,31 +2,27 @@ import { Component } from "react";
 
 class Baloons extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
       color: "#" + Math.floor(Math.random() * 16777215).toString(16),
       size: Math.trunc(Math.random() * (200 - 40 + 1) + 30),
       x: Math.trunc(Math.random() * (80 - 1 + 1) + 1),
       y: Math.trunc(Math.random() * (80 - 1 + 1) + 1),
-      counter: 0,
     };
   }
 
   componentDidMount() {}
 
   click = () => {
-    console.log(this.state.counter);
-
     this.setState({
       hide: "none",
-      counter: this.state.counter + 1,
     });
+    this.props.handlescore();
   };
 
   render() {
     return (
       <>
-        {this.state.counter}
         <div
           className="apskritimai"
           onClick={this.click}
